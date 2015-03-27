86b7c2a875b21e79958dc68f99889d45ce97fd4d
Doug <djmacdonald@uchicago.edu>
2015-03-27 12:28:18 -0400

    a

---
+++ app/scripts/directives/NavbarDirective.js
@@ -0,0 +1,12 @@
+'use strict';
+angular.module('MainDirective').directive('gaNavbar', [function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'views/navbar.html',
+    controller: 'NavbarController',
+    controllerAs: 'navbarController',
+    bindToController: true,
+    scope: {},
+    link: function($scope, element, attrs){}
+  };
+}]);
